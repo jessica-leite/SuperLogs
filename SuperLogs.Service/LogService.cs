a7aea06b53866ea14d954c4c57cec1f0b416a593
Breno Felipe <brenofeliped15@live.com>
2020-07-10 17:03:18 -0300

    criada IlogService interface, metodos de busca

--- SuperLogs.Service/LogService.cs
+++ SuperLogs.Service/LogService.cs
@@ -1,10 +1,11 @@
 ﻿using SuperLogs.Model;
 using SuperLogs.Model.Context;
 using SuperLogs.Transport;
+using System.Collections.Generic;
 
 namespace SuperLogs.Service
 {
-    public class LogService
+    public class LogService : ILogService
     {
         private AppDbContext _context;
 
@@ -12,6 +13,52 @@ namespace SuperLogs.Service
         {
             _context = context;
         }
+
+        public IList<Log> BuscaPorData(int data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorEventos(int eventos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorHostName(string hostName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorIdAmbiente(int idAmbiente)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorIdStatus(int idStatus)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorIdTipoLog(int idTipoLog)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorIdUsuario(int idUsuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<Log> BuscaPorTitulo(string titulo)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public void Criar(CriarLogDto log)
         {
             _context.Log.Add(ParaLog(log));
